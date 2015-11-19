bca75f0fe23c2ba6f40fd91f6c58d6340ad14cde
phamnhatcn06 <phamnhat.cn06@gmail.com>
2015-11-19 08:16:38 +0700

    update core

---
+++ wp-content/themes/Flexible/footer.php
@@ -0,0 +1,51 @@
+</div>    <!-- end #main-area -->
+<footer>
+    <nav id="bottomNav">
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'primary-menu',
+                'container' => '',
+                'menu_class' => 'slimmenu2',
+            )
+        );
+        ?>
+    </nav>
+    <div class="wrapper">
+        <div class="contact">
+            <p>Contact us:</p>
+            <ul>
+                <li>113 Melon Street, Braybrook 3019</li>
+                <li>Phone: 03 9317 5610</li>
+                <li>Email: office@bmnh.org.au</li>
+                <li>ABN 73 967 833 454</li>
+            </ul>
+        </div>
+        <div class="openHour">
+            <p>open hour:</p>
+            <ul>
+                <li>Monday to Thursday 9:00 am - 4:00 pm</li>
+            </ul>
+        </div>
+        <div class="social">
+            <p>Follow Us on</p>
+            <a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/icon-face.png" alt="Facebook" title="Facebook"></a>
+            <a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/icon-twitter.png" alt="Twitter" title="Twitter"></a>
+            <a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/icon-g+.png" alt="Google+" title="Google+"></a>
+        </div>
+    </div>
+    <div class="copyright">
+        <div>
+            <p class="left">Copyright 2015 @ 2015 B.M.N.H. All Rights Reserved</p>
+
+            <p class="right">Power by <img src="<?php echo get_template_directory_uri(); ?>/img/silk.png" alt="" title=""></p>
+        </div>
+    </div>
+    <img id="toTop" src="<?php echo get_template_directory_uri(); ?>/img/icon-scroll.png" alt="Scroll to top" title="Scroll to top" class="desktop">
+</footer>
+<!--<footer id="main_footer" class="clearfix">-->
+<!--    <p id="copyright">--><?php //printf(__('Designed by %s | Powered by %s', 'Flexible'), '<a href="http://www.elegantthemes.com" title="Premium WordPress Themes">Elegant Themes</a>', '<a href="http://www.wordpress.org">WordPress</a>'); ?><!--</p>-->
+<!--</footer> <!-- end #main_footer -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/Flexible/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html xmlns="http://www.w3.org/1999/xhtml" id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html xmlns="http://www.w3.org/1999/xhtml" id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html xmlns="http://www.w3.org/1999/xhtml" id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <?php elegant_description(); ?>
+    <?php elegant_keywords(); ?>
+    <?php elegant_canonical(); ?>
+
+    <?php do_action('et_head_meta'); ?>
+
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css"/>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
+
+    <!--[if lt IE 7]>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/ie6style.css"/>
+    <![endif]-->
+    <!--[if IE 7]>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/ie7style.css"/>
+    <![endif]-->
+    <!--[if IE 8]>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/ie8style.css"/>
+    <![endif]-->
+    <!--[if lt IE 9]>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+    <![endif]-->
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.11.1.min.js" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/less.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.easing.min.js" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.slimmenu.js" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jssor.js" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jssor.slider.js" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/slider-config.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        document.documentElement.className = 'js';
+    </script>
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header id="header">
+    <div class="wrapper">
+        <p class="slogan">The Braybrook Maidstone Neighbourhood House Inc</p>
+        <p class="donate"><a
+                href="#"><?php if (($phone_num = et_get_option('flexible_phone_num')) && '' != $phone_num) echo '<span id="phone">' . $phone_num . '</span>' ?></a>
+        </p>
+        <h1 id="logo"><a
+                href="<?php echo esc_url(home_url('/')); ?>"><?php if ('false' == et_get_option('flexible_logo_image', 'false') || '' == et_get_option('flexible_logo')) echo apply_filters('et_logo_markup', $bloginfo_name); else echo '<img src="' . esc_url(et_get_option('flexible_logo')) . '" alt="' . esc_attr($bloginfo_name) . '" />'; ?></a>
+        </h1>
+        <div class="formSearch">
+            <form>
+                <input type="text" placeholder="Search..." class="search_top">
+                <input type="submit" name="submit" value="Search">
+            </form>
+        </div>
+    </div>
+    <nav id="topNav">
+        <?php
+        wp_nav_menu(
+            array(
+                'theme_location' => 'primary-menu',
+                'container' => '',
+                'menu_class' => 'slimmenu',
+            )
+        );
+        ?>
+    </nav>
+</header>
+<div id="main-area">
+<?php do_action('et_header_menu'); ?>
