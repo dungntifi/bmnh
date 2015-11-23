959453b054209cf6a55c41805406756fdffc9d9e
phamnhatcn06 <phamnhat.cn06@gmail.com>
2015-11-23 21:53:35 +0700

    Nhat: commit fix bug  contact form

--- wp-content/themes/Flexible/category.php
+++ wp-content/themes/Flexible/category.php
@@ -2,9 +2,13 @@
 <div class="wrapper">
     <?php get_template_part('includes/breadcrumbs', 'page'); ?>
     <session class="mainContent category">
-        <?php while (have_posts()) : the_post(); ?>
-            <?php get_template_part('content', 'blog'); ?>
-        <?php endwhile; ?>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <?php get_template_part('content', 'blog'); ?>
+            <?php endwhile; ?><?php else:
+            ?>
+            <img src="<?php echo get_bloginfo('stylesheet_directory')  ?>/img/website_updates.jpg" title="updating website"/>
+        <?php endif; ?>
     </session>
     <?php get_sidebar(); ?>
 </div>
